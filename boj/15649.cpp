89418fa8f60e33970b694552bc8a7c7075bd87f8
nkwook <ruddnrdl@kaist.ac.kr>
2021-07-16 00:27:35 +0900

    backtracking

---
+++ boj/15649.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+int used[10];
+
+
+void backtrack(int k){
+    if(k==m){   
+        for(int i=0;i<m;i++){
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=1;i<=n;i++){
+        if(!used[i]){
+            arr[k]=i;
+            used[i]=1;
+            backtrack(k+1);
+            used[i]=0;
+        }
+    }
+    return;
+}
+
+int main(){
+    cin >> n >> m;
+
+    backtrack(0);
+}
