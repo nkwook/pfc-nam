71ba64c2e223197bbd7565be901d0c78671557e3
nkwook <ruddnrdl@kaist.ac.kr>
2021-12-22 00:37:49 +0900

    graph

--- boj/5214.cpp
+++ boj/5214.cpp
@@ -1,13 +1,16 @@
 using namespace std;
 #include <iostream>
 #include <vector>
+#include <queue>
 
 // 어렵군... 담에 정복하기로
 
 int n, m, k;
-vector<int> adjS[100001];
-vector<int> adjH[1001];
+vector<int> adj[101003];
+// vector<int> adjH[1001];
 int s;
+queue<int> Q;
+int dist[101003]={};
             
 int main(){
     cin >> n >> k >> m ;
@@ -15,11 +18,39 @@ int main(){
     for(int i=1;i<=m;i++){
         for(int j=1;j<=k;j++){
             cin >> s;
-            adjS[s].push_back(i);
-
-
+            adj[s].push_back(i+n);
+            adj[n+i].push_back(s);
+        }
+    }    
+    Q.push(1);
+    dist[1]=1;
+    int node;
+    bool succeed=false;
+    while(!Q.empty()){
+        node=Q.front();
+        Q.pop();
+        
+        if(node==n){
+            succeed=true;
+            break;
         }
 
-    }    
+        for(auto t: adj[node]){
+            if(dist[t]>0) continue;
+            dist[t]=dist[node]+1;
+            Q.push(t);
+        }
+
+    }
+    
+
+    if(n==1) cout << 1;
+    else if(!succeed) cout << -1;
+    else cout << dist[node]/2+1;
+    
+
+    
+
+
 
 }
